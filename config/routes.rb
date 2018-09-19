38543aec5fdd32d90b475fc95be4606adfa3880e
anguye68 <anguye68@masonlive.gmu.edu>
2018-09-19 17:35:23 -0400

    Added user sign up, user profiles, and validation feedback

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'users/new'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
